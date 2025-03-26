502d35ffa762e2f25d819932320e166b217e3acb
SheenyKates <dagalasheenykates@gmail.com>
2025-03-26 23:31:00 +0800

    First Push to Repository

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
@@ -11,6 +12,10 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+
+   use ApiResponser;
+
+
     /**
      * A list of the exception types that should not be reported.
      *
