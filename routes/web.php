f706cc424e01d012a9d459b09cad597edf725180
SheenyKates <dagalasheenykates@gmail.com>
2025-04-11 16:00:48 +0800

    Your commit message

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-// User routes
-$router->get('/users', 'UserController@index'); // get all users
-$router->post('/users', 'UserController@addUser'); // create new user
-$router->get('/users/{id}', 'UserController@show'); // get user by id
-$router->put('/users/{id}', 'UserController@update'); // update user
-$router->patch('/users/{id}', 'UserController@update'); // update user
-$router->delete('/users/{id}', 'UserController@delete'); // delete user
+$router->get('/users', ['uses' => 'UserController@getUsers']);
 
-// Job routes
-$router->get('/usersjob', 'UserJobController@index');
-$router->get('/usersjob/{id}', 'UserJobController@show');
+    $router->get('/users', 'UserController@index');//get all users records
+    $router->post('/users', 'UserController@addUser');//create new user record
+    $router->get('/users/{id}', 'UserController@show');//get user by id
+    $router->put('/users/{id}', 'UserController@update');//update user record
+    $router->patch('/users/{id}', 'UserController@update');//update user record
+    $router->delete('/users/{id}', 'UserController@delete');//delete record
+
+    $router->get('/usersjob', 'UserJobController@index'); // Get all jobs
+    $router->get('/usersjob/{id}', 'UserJobController@show'); // Get job by IDs
+
